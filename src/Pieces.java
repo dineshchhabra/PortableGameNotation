b956cb3d9d67df64c32df9dacba4a0797d459e3f
Sonia V Mathew <sonisvm@gmail.com>
2015-07-29 15:02:56 +0530

    added constructor to Pieces

--- src/Pieces.java
+++ src/Pieces.java
@@ -1,8 +1,12 @@
 
  abstract class Pieces {
 	private Dimension pos;
-	private bool vaild;
+	private boolean valid;
 
+	Pieces(){
+		valid = true;
+	}
+	
     public abstract boolean movePosible(Dimension dstCoordinates);
     public abstract void move(Dimension dstCoordinates);
     public abstract void setPosition(Dimension dstCoordinates);
