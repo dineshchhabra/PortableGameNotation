1f8739ab0ab849984f5788f14314d5713a361eb1
Sonia V Mathew <sonisvm@gmail.com>
2015-07-29 15:18:02 +0530

    added queen class

--- src/Queen.java
+++ src/Queen.java
@@ -1,18 +1,18 @@
 
-public class Queen {
+public class Queen extends Pieces{
 	@Override
 	public boolean movePossible(Dimension dstCoordinates) {
 		
 		
-		if(Math.abs(dstCoordinates.getx()-this.pos.getx())==1 && Math.abs(dstCoordinates.getx()-this.pos.getx())==1)
+		if(Math.abs(dstCoordinates.getx()-this.getDimension().getx())>=1 && Math.abs(dstCoordinates.gety()-this.getDimension().gety())==0)
 		{
 			return true;
 		}
-		else if(Math.abs(dstCoordinates.getx()-this.pos.getx())==1 && Math.abs(dstCoordinates.getx()-this.pos.getx())==0)
+		else if(Math.abs(dstCoordinates.gety()-this.getDimension().gety())>=1 && Math.abs(dstCoordinates.getx()-this.getDimension().getx())==0)
 		{
 			return true;
 		}
-		else if(Math.abs(dstCoordinates.getx()-this.pos.getx())==0 && Math.abs(dstCoordinates.getx()-this.pos.getx())==1)
+		else if(Math.abs(dstCoordinates.getx()-this.getDimension().getx())== Math.abs(dstCoordinates.gety()-this.getDimension().gety()))
 		{
 			return true;
 		}
@@ -24,21 +24,22 @@ public class Queen {
 
 	@Override
 	public void move(Dimension dstCoordinates) {
-		// TODO Auto-generated method stub
 		
-		this.pos.set(dstCoordinates.getx(), dstCoordinates.gety());
+		this.getDimension().set(dstCoordinates.getx(), dstCoordinates.gety());
 		
 	}
 
 	@Override
 	public void setPosition(Dimension dstCoordinates) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 	@Override
-	public void setPosition(Dimension dstCoordinates) {
-		
-		
+	 public void captured(Dimension dstCoordinates){
+		if(this.getDimension().getx() == dstCoordinates.getx() && this.getDimension().gety() == dstCoordinates.gety()){
+			this.setValid(false);
+		}
 	}
+	
 
 }
