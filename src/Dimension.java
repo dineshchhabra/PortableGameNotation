d66d6f6b940307ef8b123a5b477a631fa77dda9e
dineshchhabra <dinesh.chha11@gmail.com>
2015-07-29 13:01:20 +0530

    added Dimensions.java

---
+++ src/Dimension.java
@@ -0,0 +1,14 @@
+class Dimension {
+	 int x, y; 
+     public void set(int x, int y) {
+    	 this.x=x;
+    	 this.y=y;
+     }
+     public int getx(){
+    	 return x;
+     }
+
+     public int gety(){
+    	 return y;
+     }
+}
--- src/Pieces.java
+++ src/Pieces.java
@@ -1,4 +1,8 @@
 
-public class Pieces {
+ abstract class Pieces {
+	Dimension pos;
 
+    public abstract boolean movePosible(Dimension dstCoordinates);
+    public abstract void move(Dimension dstCoordinates);
+    public abstract void setPosition(Dimension dstCoordinates);
 }
