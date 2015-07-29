a76eafeb851d3d399b47261870699a6f2434bce9
Sonia V Mathew <sonisvm@gmail.com>
2015-07-29 15:24:54 +0530

    added Rook class

--- src/Rook.java
+++ src/Rook.java
@@ -1,4 +1,41 @@
 
-public class Rook {
+public class Rook extends Pieces{
+	@Override
+	public boolean movePossible(Dimension dstCoordinates) {
+		
+		
+		if(Math.abs(dstCoordinates.getx()-this.getDimension().getx())>=1 && Math.abs(dstCoordinates.gety()-this.getDimension().gety())==0)
+		{
+			return true;
+		}
+		else if(Math.abs(dstCoordinates.gety()-this.getDimension().gety())>=1 && Math.abs(dstCoordinates.getx()-this.getDimension().getx())==0)
+		{
+			return true;
+		}
+		
+		else
+		{
+			return false;
+		}
+	}
+
+	@Override
+	public void move(Dimension dstCoordinates) {
+		
+		this.getDimension().set(dstCoordinates.getx(), dstCoordinates.gety());
+		
+	}
+
+	@Override
+	public void setPosition(Dimension dstCoordinates) {
+		
+		
+	}
+	@Override
+	 public void captured(Dimension dstCoordinates){
+		if(this.getDimension().getx() == dstCoordinates.getx() && this.getDimension().gety() == dstCoordinates.gety()){
+			this.setValid(false);
+		}
+	}
 
 }
