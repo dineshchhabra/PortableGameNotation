8f1c65e1457dea2eabe76bb90747dfc7f741dd4d
unknown <banerjee.soumyajyoti4@gmail.com>
2015-07-30 11:19:25 +0530

    Updated test cases

--- src/ChessBoardTest.java
+++ src/ChessBoardTest.java
@@ -13,6 +13,10 @@ public class ChessBoardTest {
 		assertEquals(true,testObject.isEndCondition("1-0"));
 		assertEquals(true,testObject.isEndCondition("0-1"));
 		assertEquals(true,testObject.isEndCondition("1/2-1/2"));
+		
+		assertEquals("Match Draw",testObject.gameResult("1/2-1/2"));
+		assertEquals("Black win",testObject.gameResult("0-1"));
+		assertEquals("White win",testObject.gameResult("1-0"));
 	}
 
 }
